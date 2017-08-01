d31e834f9e9555537b4ace2985fd60398aea3532
Ильяс <i.almakaev@gmail.com>
2017-08-01 15:45:48 +0700

    Initial Commit

---
+++ Patterns_7_Bridge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Patterns_7_Bridge
+//
+//  Created by Ильяс on 01.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
